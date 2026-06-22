95d3827d0cf406dca75a38b347e9a32fa28e892f
Jenil <25je1085@iitism.ac.in>
2026-06-22 17:25:47 +0530

    changes saved successfully

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //req.body
+const PORT = process.env.PORT || 3000; 
+
 const MenuItem = require('./models/MenuItem');
 
 app.get('/', (req, res) => {
@@ -20,7 +23,6 @@ app.use('/person',personRoutes);
 
 const menuItemRoutes = require('./routes/menuItemRoutes');
 app.use('/menu',menuItemRoutes);
-
 
 
 app.listen(3000, () => {
